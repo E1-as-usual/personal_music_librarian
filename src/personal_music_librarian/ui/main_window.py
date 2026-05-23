ffa0dc8d8d35db1d0a55c5b8cb95cfbf79455bca
Ioan Chiurciu <ioan@chiurciu.com>
2026-05-23 06:48:50 +0300

    Add metadata editor navigation

--- src/personal_music_librarian/ui/main_window.py
+++ src/personal_music_librarian/ui/main_window.py
@@ -5,6 +5,7 @@ from PySide6.QtWidgets import QToolBar
 from personal_music_librarian.ui.pages.duplicates_page import DuplicatesPage
 from personal_music_librarian.ui.pages.history_page import HistoryPage
 from personal_music_librarian.ui.pages.library_page import LibraryPage
+from personal_music_librarian.ui.pages.metadata_editor_page import MetadataEditorPage
 from personal_music_librarian.ui.pages.rename_preview_page import RenamePreviewPage
 
 
@@ -19,12 +20,14 @@ class MainWindow(QMainWindow):
         self.duplicates_page = DuplicatesPage()
         self.rename_preview_page = RenamePreviewPage()
         self.history_page = HistoryPage()
+        self.metadata_editor_page = MetadataEditorPage()
 
         self.stack = QStackedWidget()
         self.stack.addWidget(self.library_page)
         self.stack.addWidget(self.duplicates_page)
         self.stack.addWidget(self.rename_preview_page)
         self.stack.addWidget(self.history_page)
+        self.stack.addWidget(self.metadata_editor_page)
 
         toolbar = QToolBar("Navigation")
         self.addToolBar(toolbar)
@@ -47,6 +50,11 @@ class MainWindow(QMainWindow):
         history_action = toolbar.addAction("History")
         history_action.triggered.connect(
             lambda: self.stack.setCurrentWidget(self.history_page)
+        )
+
+        metadata_action = toolbar.addAction("Metadata")
+        metadata_action.triggered.connect(
+            lambda: self.stack.setCurrentWidget(self.metadata_editor_page)
         )
 
         self.setCentralWidget(self.stack)
