3ddf7cc27fa3e0c0fca8645c15a1c06ba784a55b
Ioan Chiurciu <ioan@chiurciu.com>
2026-05-23 06:40:03 +0300

    Add history navigation

--- src/personal_music_librarian/ui/main_window.py
+++ src/personal_music_librarian/ui/main_window.py
@@ -3,6 +3,7 @@ from PySide6.QtWidgets import QStackedWidget
 from PySide6.QtWidgets import QToolBar
 
 from personal_music_librarian.ui.pages.duplicates_page import DuplicatesPage
+from personal_music_librarian.ui.pages.history_page import HistoryPage
 from personal_music_librarian.ui.pages.library_page import LibraryPage
 from personal_music_librarian.ui.pages.rename_preview_page import RenamePreviewPage
 
@@ -17,11 +18,13 @@ class MainWindow(QMainWindow):
         self.library_page = LibraryPage()
         self.duplicates_page = DuplicatesPage()
         self.rename_preview_page = RenamePreviewPage()
+        self.history_page = HistoryPage()
 
         self.stack = QStackedWidget()
         self.stack.addWidget(self.library_page)
         self.stack.addWidget(self.duplicates_page)
         self.stack.addWidget(self.rename_preview_page)
+        self.stack.addWidget(self.history_page)
 
         toolbar = QToolBar("Navigation")
         self.addToolBar(toolbar)
@@ -39,6 +42,11 @@ class MainWindow(QMainWindow):
         rename_action = toolbar.addAction("Rename Preview")
         rename_action.triggered.connect(
             lambda: self.stack.setCurrentWidget(self.rename_preview_page)
+        )
+
+        history_action = toolbar.addAction("History")
+        history_action.triggered.connect(
+            lambda: self.stack.setCurrentWidget(self.history_page)
         )
 
         self.setCentralWidget(self.stack)
