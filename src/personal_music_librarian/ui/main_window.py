5508f7ed7f1deff7b9f176540b204d3e321fa622
Ioan Chiurciu <ioan@chiurciu.com>
2026-05-23 06:22:08 +0300

    Add duplicate review navigation

--- src/personal_music_librarian/ui/main_window.py
+++ src/personal_music_librarian/ui/main_window.py
@@ -1,5 +1,8 @@
 from PySide6.QtWidgets import QMainWindow
+from PySide6.QtWidgets import QStackedWidget
+from PySide6.QtWidgets import QToolBar
 
+from personal_music_librarian.ui.pages.duplicates_page import DuplicatesPage
 from personal_music_librarian.ui.pages.library_page import LibraryPage
 
 
@@ -11,5 +14,23 @@ class MainWindow(QMainWindow):
         self.resize(1400, 900)
 
         self.library_page = LibraryPage()
+        self.duplicates_page = DuplicatesPage()
 
-        self.setCentralWidget(self.library_page)
+        self.stack = QStackedWidget()
+        self.stack.addWidget(self.library_page)
+        self.stack.addWidget(self.duplicates_page)
+
+        toolbar = QToolBar("Navigation")
+        self.addToolBar(toolbar)
+
+        library_action = toolbar.addAction("Library")
+        library_action.triggered.connect(
+            lambda: self.stack.setCurrentWidget(self.library_page)
+        )
+
+        duplicates_action = toolbar.addAction("Duplicates")
+        duplicates_action.triggered.connect(
+            lambda: self.stack.setCurrentWidget(self.duplicates_page)
+        )
+
+        self.setCentralWidget(self.stack)
